aee96a35a410856464bc1d5bde6f14b94807dba8
sakthiakash2007 <sakthiakash.250068@csbs.ritchennai.edu.in>
2026-09-09 15:05:54 +0530

    Add files via upload

---
+++ programm 4.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+
+        temp.next = new_node
+
+    def display(self):
+        temp = self.head
+
+        while temp:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+
+        print("None")
+
+
+ll = SinglyLinkedList()
+
+ll.insert(10)
+ll.insert(20)
+ll.insert(30)
+
+ll.display()
